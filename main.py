b3d6ff6bc6374902aac8185ac82ea970aebc6a91
Islam <bekov2557@gmail.com>
2026-03-27 18:42:40 +0300

    Update 64 players with new nicknames per group

--- main.py
+++ main.py
@@ -44,28 +44,28 @@ PLAYOFF_CONFIG = {'topic_name': 'ПЛЕЙ-ОФФ РЕЗУЛЬТАТЫ'}
 
 PLAYERS = {
     'Группа A': [
-        'Tribalchief_OTC1', 'shtorm10', 'TrunksFC', 'Vadim260401',
-        'Danis1985_RM', 'PrincepsInferni_Flavoculus', 'yura192883', 'Annndreiii',
-        'vtrrgyg', 'GodFatherVSA', 'NotEterna1', 'legolas422', 'Vit88',
-        'visshera', 'YarcheeVsa', 'bad_606'
+        'ОТС', 'Шторм', 'Китаец', 'RealMadrid',
+        'Danis RM', 'AR-Unlucky', 'AR-Timeless', 'Beshenyi',
+        'Bumblebee', 'Ar-iceberg', 'NotEternal', 'Енот',
+        '116Rus', 'Vishera', 'JLбычый', 'Daredevil'
     ],
     'Группа B': [
-        'kaistroden', 'prostotip550', 'Andrey_17A', 'joeyhuichan',
-        'Dr_Wh11te', 'Yur4ik_1987', 'FcTaganrog', 'mihasik400',
-        'tricky938', 'ded1747n', 'shaut', 'likshonn',
-        'arnurchik17', 'alexmagenta', 'platonm09', 'Anatolyveniva4'
+        'zloy', 'Ростас', 'GLAcapelllkzxc', 'alz',
+        'V1RɄS', 'Zenit', 'Т卂Г卂Н卩ОГツ', 'Mihasik',
+        'ЧерныйНеегрр', 'дедскв', 'gg', 'likshonn',
+        'nsGUW-117', 'Маджента', 'Cristiano2828', 'AnatoliyIva4'
     ],
     'Группа C': [
-        'sp1r1tVSA', 'Arm032', 'KesarTM', 'arrowsgang',
-        'StrongMannVSA', 'reconquistaR9', 'zaali_916', 'dopolnitelb',
-        'Willamsfire', 'korzhiman98', 'prostomaksx', 'NNovitskiy',
-        'freezy_66', 'gatalskiy58', 'onvamnemihaaa', 'Serghe1KO'
+        'Yarchee', 'SoEz', '(FCSM) Спартач', 'RTOTY-WIZARD',
+        'Strongmann', 'RonaldoR9', 'GlGreshnik', 'zenit78',
+        'GLDJAMBORZ', 'КОРЖИМАН', 'GLAngarsk', 'SEEAL',
+        'Freezy', 'Черти58', '3lodeu', 'Seriu'
     ],
     'Группа D': [
-        'RomaS93', 'Yerb0ll', 'artsmilex', 'Moes1k',
-        'Sergo1233', 'blancos_15', 'Azat72172', 'Vital0587',
-        'ShakhtarDonetsk56', 'ConstantinoXIII', 'shomashoma98', 'Frost_5454',
-        'KDI_BY', 'S_G_A_MVP', 'karbon0_0', 'Aleksei19811'
+        'Старушка Изольда', 'nsMor1arty', 'artsmile', 'Guess Who',
+        'Лизун0', 'GLBek_07', 'БЕЗБАШЕННЫЙ', 'VitalyRus',
+        'kOFFe', 'CONSTANTINO', 'DeLPaPa', 'Exclusive',
+        'ミКРУПЬЕ彡', 'LazyMaxx', 'Karbon', 'ШинникЯр'
     ]
 }
 
